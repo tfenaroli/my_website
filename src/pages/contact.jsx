db8d873e62e17efb08e6bfe0f003e055064a8c45
Thomas Fenaroli <tfenaroli@icloud.com>
2022-03-08 18:53:19 -0500

    added text wrapping to contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -6,17 +6,19 @@ const Contact = () => {
         <React.Fragment>
             <Row className="mt-5">
                 <Col className="text-center">
-                    <h1>thomas.s.fenaroli.24@dartmouth.edu</h1>
+                    <h1 className="text-wrap">
+                        thomas.s.fenaroli.24@dartmouth.edu
+                    </h1>
                 </Col>
             </Row>
             <Row className="mt-5">
                 <Col className="text-center">
-                    <h1>tfenaroli@icloud.com</h1>
+                    <h1 className="text-wrap">tfenaroli@icloud.com</h1>
                 </Col>
             </Row>
             <Row className="mt-5">
                 <Col className="text-center">
-                    <h1>(203)-832-5002</h1>
+                    <h1 className="text-wrap">(203)-832-5002</h1>
                 </Col>
             </Row>
         </React.Fragment>
